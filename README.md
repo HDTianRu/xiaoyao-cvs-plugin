6d4976b96864ab0f0ebcfa8195f9a180065bbf98
HDTianRu <3291691454@qq.com>
2024-08-21 14:59:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# xiaoyao-cvs-plugin
+
+改版自用，去除图鉴功能，保留sk，ck等功能
+
+## 安装
+使用gitee
+```sh
+git clone https://gitee.com/HDTianRu/xiaoyao-cvs-plugin.git ./plugins/xiaoyao-cvs-plugin/
+```
+使用github
+```sh
+git clone https://github.com/HDTianRu/xiaoyao-cvs-plugin.git ./plugins/xiaoyao-cvs-plugin/
+```
+
+## 鸣谢
+- https://gitee.com/Ctrlcvs/xiaoyao-cvs-plugin
+- https://gitee.com/bbaban/xiaoyao-cvs-plugin
+
+## 其他
+- 本仓库随缘更新，出bug可能没能力修
+- [原作者C佬发电](https://afdian.net/a/Ctrlcvs)
---
+++ index.js
@@ -0,0 +1,42 @@
+// 适配V3 Yunzai，将index.js移至app/index.js
+import {
+	currentVersion,
+	isV3
+} from './components/Changelog.js'
+import Data from './components/Data.js'
+
+if (!global.segment) {
+  global.segment = (await import("oicq")).segment
+}
+
+export * from './apps/index.js'
+let index = {
+	atlas: {}
+}
+if (isV3) {
+	Bot.logger=logger
+	index = await Data.importModule('/plugins/xiaoyao-cvs-plugin/adapter', 'index.js')
+}
+
+export const atlas = index.atlas || {}
+
+Bot.logger.info(`---------^_^---------`)
+Bot.logger.info(`图鉴插件${currentVersion}初始化~`)
+
+setTimeout(async function() {
+	let msgStr = await redis.get('xiaoyao:restart-msg')
+	let relpyPrivate = async function() {}
+	if (!isV3) {
+		let common = await Data.importModule('/lib', 'common.js')
+		if (common && common.default && common.default.relpyPrivate) {
+			relpyPrivate = common.default.relpyPrivate
+		}
+	}
+	if (msgStr) {
+		let msg = JSON.parse(msgStr)
+		await relpyPrivate(msg.qq, msg.msg)
+		await redis.del('xiaoyao:restart-msg')
+		let msgs = [`当前图鉴版本: ${currentVersion}`, '您可使用 #图鉴版本 命令查看更新信息']
+		await relpyPrivate(msg.qq, msgs.join('\n'))
+	}
+}, 1000)
